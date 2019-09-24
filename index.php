1f4660918bb5b273d32271323661d139b6a78c62
ekalm <ekaluthfiana29@gmail.com>
2019-09-24 11:54:16 +0700

    bootstrap

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+       <title>Latihan Bootstrap</title>
+       <!--Pemanggilan CSS-->
+       <link rel ="stylesheet" href="/assets/css/bootstrap.grid.min.css"/>
+        
+       <!--Akhir Pemanggilan CSS--> 
+
+</head>
+
+
+<body>
+     <!-- Header -->
+     <!-- End Header -->
+
+     <!-- Content -->
+     <div class="contaainer">
+        <div class="row">
+           <div class ="col-md-12" style="background:violet">
+               <h1>12 Column </h1>
+          </div>
+        </div>
+     <div class="row">
+         <div class="col-md-6" style="background:yellow">
+               <h1>6 Column</h1>
+           <div>
+         <div class="col-md-6" style="background:blue">
+               <h1>6 Column</h1>
+           </div>
+       </div>
+     <!-- End row -->
+     </div>
+
+     <!-- End Content -->
+
+     <!-- Footer -->
+     <!-- End Footer -->
+
+     <!-- Pemnanggilan JS -->
+     <script src="/assets/js/bootstrap.bundle.js"></script>
+     <script src="/assets/js/bootstrap.bundle.min.js"></script>
+     <!-- Akhir Pemanggilan Js -->
+</body>
+</html>
